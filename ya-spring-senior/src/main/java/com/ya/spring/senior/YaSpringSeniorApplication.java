4deae7b4a8b711790cdac42aa7343e59c9126b19
AdyRol <loyail@163.com>
2020-08-16 14:39:27 +0800

    spring高级部分新建项目

---
+++ ya-spring-senior/src/main/java/com/ya/spring/senior/YaSpringSeniorApplication.java
@@ -0,0 +1,13 @@
+package com.ya.spring.senior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YaSpringSeniorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YaSpringSeniorApplication.class, args);
+    }
+
+}
---
+++ ya-spring-senior/src/test/java/com/ya/spring/senior/YaSpringSeniorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ya.spring.senior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YaSpringSeniorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
